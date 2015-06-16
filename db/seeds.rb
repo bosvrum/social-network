06d80960c7e477168059e546fbbe6ef602d5a566
Catalin <catalinboruga@yahoo.com>
2015-06-16 13:44:32 +0200

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(username: "bosvrum01", email: "catalinboruga@yahoo.com", password: "nemuritorul", password_confirmation: "nemuritorul")
+User.create(username: "bosvrum02", email: "catalinboruga@yahoo.com", password: "nemuritorul", password_confirmation: "nemuritorul")
+User.create(username: "bosvrum03", email: "catalinboruga@yahoo.com", password: "nemuritorul", password_confirmation: "nemuritorul")
+User.create(username: "bosvrum04", email: "catalinboruga@yahoo.com", password: "nemuritorul", password_confirmation: "nemuritorul")
+User.create(username: "bosvrum05", email: "catalinboruga@yahoo.com", password: "nemuritorul", password_confirmation: "nemuritorul")
+p "Test users created"
